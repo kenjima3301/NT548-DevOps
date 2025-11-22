bcf652c38f23b415d320bf7dd130cdf333513c1a
kenjima3301 <danhtran3301@gmail.com>
2025-11-22 09:17:07 +0700

    feat: bootstrap production environment with isolated state and tightened security

---
+++ terraform-aws-iac/envs/production/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "nt548-my-tfstate-bucket-2025-unique"
+    key    = "production/terraform.tfstate"
+    region = "ap-southeast-1"
+
+    use_lockfile = true
+  }
+}
---
+++ terraform-aws-iac/envs/production/output.tf
@@ -0,0 +1,9 @@
+output "web_server_public_ip" {
+  description = "Public IP of the web server in dev"
+  value       = module.ec2.public_ip
+}
+
+output "ecr_repository_url" {
+  description = "URL of ECR store to push image"
+  value       = aws_ecr_repository.my_app.repository_url
+}
